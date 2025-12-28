10945315ff6777cf5745a5c42742d7e292ca08d7
Headliner770 <paul248022@gmail.com>
2025-12-28 22:23:23 +0300

    ContactUs

--- src/components/contact/index.tsx
+++ src/components/contact/index.tsx
@@ -1,3 +1,7 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { contactSchema, contactFormType } from "@/utils/contactScema";
+import ContactUsPageGraphic from "@assets/ContactUsPageGraphic.png";
+
 const ContactUs = () => {
   return (
     <section id="contactus" className="mx-auto w-5/6 pb-32 pt-24">
